3fc388927074aa2c198346b56face1c548c5ddf4
rewico <codergq@gmail.com>
2022-11-03 14:58:13 +0500

    added fetch API, some datas, a bit style

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const elDivDetails = document.getElementById("details");
+const elDivImgContainer = document.getElementById("img-container");
+const elBtnGetUser = document.getElementById("next-random-user");
+const URL = "https://random-data-api.com/api/v2/users";
+
+const getUser = () => {
+  const renderUser = (data) => {
+    console.log(data);
+    elDivImgContainer.innerHTML = `<img src="${data.avatar}">`;
+    elDivDetails.innerHTML = `
+    <h2>${data.first_name} ${data.last_name}</h2>
+    <h3>${data.employment.title}</h3>
+    <h4>${data.address.city}</h4>
+    `;
+
+    let randomColor =
+      "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
+
+    document.documentElement.style.setProperty("--theme-color", randomColor);
+    document.body.style.backgroundColor = randomColor;
+    console.log(randomColor);
+  };
+
+  try {
+    (async () => {
+      const RESPONSE = await fetch(URL);
+      const DATA = await RESPONSE.json();
+      renderUser(DATA);
+    })();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+window.addEventListener("load", getUser);
+elBtnGetUser.addEventListener("click", getUser);
